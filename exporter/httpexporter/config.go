8f60f87037fda2ca9b5a8d4c395f0f6a1e07eda3
Simon Zelenski <simon.zelenski@gmail.com>
2024-10-17 18:24:09 +0200

    [exporter/http] fix config check

--- exporter/httpexporter/config.go
+++ exporter/httpexporter/config.go
@@ -17,10 +17,11 @@ type EndpointConfig struct {
 // Validate checks if the receiver configuration is valid
 // Additionally it overrides the configuration set if the respective environment variables are set
 func (cfg *Config) Validate() error {
-	if cfg.Endpoint.IP != "" {
-		if net.ParseIP(cfg.Endpoint.IP) == nil {
-			return errors.New("endpoint.ip is not a valid IP address")
-		}
+	if cfg.Endpoint.IP == "" {
+		return errors.New("endpoint.ip is required")
+	}
+	if net.ParseIP(cfg.Endpoint.IP) == nil {
+		return errors.New("endpoint.ip is not a valid IP address")
 	}
 	if cfg.Endpoint.Port > 65535 || cfg.Endpoint.Port < 1024 {
 		return errors.New("endpoint.port is not a valid port")
