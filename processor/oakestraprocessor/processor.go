0f06ecd385873d561df99031b4f8dc3ed67504dd
Simon Zelenski <simon.zelenski@gmail.com>
2024-10-24 16:19:46 +0200

    [processor/oakestra] add verbose logging for first testing

--- processor/oakestraprocessor/processor.go
+++ processor/oakestraprocessor/processor.go
@@ -7,35 +7,29 @@ import (
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/processor"
 	"go.uber.org/zap"
-	"sync"
 )
 
 var _ processor.Metrics = (*Processor)(nil)
 
 type Processor struct {
-	next consumer.Metrics
-
+	next   consumer.Metrics
 	log    *zap.Logger
-	ctx    context.Context
 	cancel context.CancelFunc
-
-	mtx sync.Mutex
 }
 
 func newProcessor(cfg *Config, logger *zap.Logger, next consumer.Metrics) *Processor {
-	ctx, cancel := context.WithCancel(context.Background())
 
 	proc := Processor{
 		next: next,
-
-		log:    logger,
-		ctx:    ctx,
-		cancel: cancel,
+		log:  logger,
 	}
 	return &proc
 }
 
-func (p *Processor) Start(_ context.Context, _ component.Host) error {
+func (p *Processor) Start(ctx context.Context, _ component.Host) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	p.cancel = cancel
+	p.log.Info("starting oakestra processor")
 	return nil
 }
 
@@ -49,8 +43,6 @@ func (p *Processor) Capabilities() consumer.Capabilities {
 }
 
 func (p *Processor) ConsumeMetrics(ctx context.Context, metrics pmetric.Metrics) error {
-	// TODO: implement metrics consumption
-	// TODO: extract metrics of interest
-	// TODO: apply mutation on metrics of interest
+	p.log.Info("metrics consumed", zap.Any("metrics", metrics))
 	return nil
 }
