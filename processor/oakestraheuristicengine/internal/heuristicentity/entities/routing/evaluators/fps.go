8b3f44e474e3f99cf08016abb372268450f7d2e9
Simon Zelenski <simon.zelenski@gmail.com>
2025-05-29 19:26:23 +0200

    [processor/heuristic] implement fps evaluator

--- processor/oakestraheuristicengine/internal/heuristicentity/entities/routing/evaluators/fps.go
+++ processor/oakestraheuristicengine/internal/heuristicentity/entities/routing/evaluators/fps.go
@@ -1,6 +1,9 @@
 package evaluators
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/smnzlnsk/opentelemetry-components/processor/oakestraheuristicengine/internal/domain"
 )
 
@@ -15,6 +18,14 @@ func NewFpsEvaluator(identifier string) domain.Evaluator {
 }
 
 func (e *FpsEvaluator) Evaluate(factor float64, arguments map[string]interface{}) (float64, error) {
+	n, ok := arguments["service_fps(0){in}"].(float64)
+	if !ok {
+		return 0, fmt.Errorf("service_fps(0){in} not found")
+	}
 
-	return 0, nil
+	return exponentialDecay(n, 0.02), nil
+}
+
+func exponentialDecay(n, k float64) float64 {
+	return 1 - math.Exp(-k*n)
 }
